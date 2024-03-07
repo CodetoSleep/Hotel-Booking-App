54452b1781c12c7a98b95dcba9d593d03e3a218c
Bùi Phương Nam <phuongnam2442003@gmail.com>
2024-03-07 09:03:17 +0700

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,24 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotelControllers.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/:id", getHotel);
+
+ //GET ALL
+router.get("/", getAllHotels);
+
+export default router;
